9abbf475c00a6085fdf5e45b5b47056fd0431af3
Dheeraj <dkbhaikar@gmail.com>
2025-03-12 23:05:39 +0530

    event update

--- event.js
+++ event.js
@@ -1,115 +1,132 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menu-icon");
   const navLinks = document.getElementById("nav-links");
+
   // let menuIcon = document.getElementById("menu-icon");
-let rotate_1=document.getElementById('rotate_1');
-let rotate_2=document.getElementById('rotate_2');
-let hide_1=document.getElementById('hide_1');
-let hide_2=document.getElementById('hide_2');
+  let rotate_1 = document.getElementById('rotate_1');
+  let rotate_2 = document.getElementById('rotate_2');
+  let hide_1 = document.getElementById('hide_1');
+  let hide_2 = document.getElementById('hide_2');
+
   menuIcon.addEventListener("click", function () {
-      navLinks.classList.toggle("show");
-      // console.log(navLinks.classList.value)
-      if (navLinks.classList.value == "nav-links show"){
-          rotate_1.style.transform = "rotate(45deg)";
-          rotate_2.style.transform = "rotate(-45deg)";
-          hide_1.style.zIndex = "-1";
-          hide_2.style.zIndex = "-1";
-      }
-      if (navLinks.classList.value == "nav-links"){
-          rotate_1.style.transform = "rotate(0deg)";
-          rotate_2.style.transform = "rotate(0deg)";
-          hide_1.style.zIndex = "0";
-          hide_2.style.zIndex = "0";
-      }
+    navLinks.classList.toggle("show");
+    // console.log(navLinks.classList.value)
+    if (navLinks.classList.value == "nav-links show") {
+      rotate_1.style.transform = "rotate(45deg)";
+      rotate_2.style.transform = "rotate(-45deg)";
+      hide_1.style.zIndex = "-1";
+      hide_2.style.zIndex = "-1";
+    }
+    if (navLinks.classList.value == "nav-links") {
+      rotate_1.style.transform = "rotate(0deg)";
+      rotate_2.style.transform = "rotate(0deg)";
+      hide_1.style.zIndex = "0";
+      hide_2.style.zIndex = "0";
+    }
+
   });
 });
 
- // Auto-sliding image slider
- const sliders = document.querySelectorAll(".ag-timeline-card_img-box");
- sliders.forEach((slider) => {
-   let images = slider.querySelectorAll("img");
-   let index = 0;
 
-   function slideImages() {
-     images.forEach(img => img.style.display = "none");
-     images[index].style.display = "block";
-     index = (index + 1) % images.length;
-   }
+// // document.getElementById("p2").style.color = "blue";
+// menuIcon.addEventListener('click',()=>{
+//     if (hide == 0){
+//         console.log('hi');
+//         rotate_1.style.transform = "rotate(-45deg)";
+//         rotate_2.style.transform = "rotate(+45deg)";
+//         hide_1.style.zIndex = "0";
+//         hide_2.style.zIndex = "0";
+//         hide = 1
+//     }    
+// });
 
-   slideImages(); // Show the first image
-   setInterval(slideImages, 3000); // Change image every 3 seconds
- });
+
+// Auto-sliding image slider
+const sliders = document.querySelectorAll(".ag-timeline-card_img-box");
+sliders.forEach((slider) => {
+  let images = slider.querySelectorAll("img");
+  let index = 0;
+
+  function slideImages() {
+    images.forEach(img => img.style.display = "none");
+    images[index].style.display = "block";
+    index = (index + 1) % images.length;
+  }
+
+  slideImages(); // Show the first image
+  setInterval(slideImages, 3000); // Change image every 3 seconds
+});
 
 
 
 (function ($) {
-    $(function () {
-  
-  
-      $(window).on('scroll', function () {
-        fnOnScroll();
-      });
-  
-      $(window).on('resize', function () {
-        fnOnResize();
-      });
-  
-  
-      var agTimeline = $('.js-timeline'),
-        agTimelineLine = $('.js-timeline_line'),
-        agTimelineLineProgress = $('.js-timeline_line-progress'),
-        agTimelinePoint = $('.js-timeline-card_point-box'),
-        agTimelineItem = $('.js-timeline_item'),
-        agOuterHeight = $(window).outerHeight(),
-        agHeight = $(window).height(),
-        f = -1,
-        agFlag = false;
-  
-      function fnOnScroll() {
-        agPosY = $(window).scrollTop();
-  
-        fnUpdateFrame();
-      }
-  
-      function fnOnResize() {
-        agPosY = $(window).scrollTop();
-        agHeight = $(window).height();
-  
-        fnUpdateFrame();
-      }
-  
-      function fnUpdateWindow() {
-        agFlag = false;
-  
-        agTimelineLine.css({
-          top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
-          bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
-        });
-  
-        f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
-      }
-  
-      function fnUpdateProgress() {
-        var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
-  
-        i = agTop + agPosY - $(window).scrollTop();
-        a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
-        n = agPosY - a + agOuterHeight / 2;
-        i <= agPosY + agOuterHeight / 2 && (n = i - a);
-        agTimelineLineProgress.css({height: n + "px"});
-  
-        agTimelineItem.each(function () {
-          var agTop = $(this).find(agTimelinePoint).offset().top;
-  
-          (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
-        })
-      }
-  
-      function fnUpdateFrame() {
-        agFlag || requestAnimationFrame(fnUpdateWindow);
-        agFlag = true;
-      }
-  
-  
+  $(function () {
+
+
+    $(window).on('scroll', function () {
+      fnOnScroll();
     });
-  })(jQuery);
+
+    $(window).on('resize', function () {
+      fnOnResize();
+    });
+
+
+    var agTimeline = $('.js-timeline'),
+      agTimelineLine = $('.js-timeline_line'),
+      agTimelineLineProgress = $('.js-timeline_line-progress'),
+      agTimelinePoint = $('.js-timeline-card_point-box'),
+      agTimelineItem = $('.js-timeline_item'),
+      agOuterHeight = $(window).outerHeight(),
+      agHeight = $(window).height(),
+      f = -1,
+      agFlag = false;
+
+    function fnOnScroll() {
+      agPosY = $(window).scrollTop();
+
+      fnUpdateFrame();
+    }
+
+    function fnOnResize() {
+      agPosY = $(window).scrollTop();
+      agHeight = $(window).height();
+
+      fnUpdateFrame();
+    }
+
+    function fnUpdateWindow() {
+      agFlag = false;
+
+      agTimelineLine.css({
+        top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
+        bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
+      });
+
+      f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
+    }
+
+    function fnUpdateProgress() {
+      var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
+
+      i = agTop + agPosY - $(window).scrollTop();
+      a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+      n = agPosY - a + agOuterHeight / 2;
+      i <= agPosY + agOuterHeight / 2 && (n = i - a);
+      agTimelineLineProgress.css({ height: n + "px" });
+
+      agTimelineItem.each(function () {
+        var agTop = $(this).find(agTimelinePoint).offset().top;
+
+        (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
+      })
+    }
+
+    function fnUpdateFrame() {
+      agFlag || requestAnimationFrame(fnUpdateWindow);
+      agFlag = true;
+    }
+
+
+  });
+})(jQuery);
